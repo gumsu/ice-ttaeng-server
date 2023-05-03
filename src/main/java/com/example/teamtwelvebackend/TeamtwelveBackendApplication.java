421b5699aad57a6040a7dd2c489aac6f255ca5e5
dsyang <neoanimato@gmail.com>
2023-05-03 20:51:40 +0900

    initial commit

---
+++ src/main/java/com/example/teamtwelvebackend/TeamtwelveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamtwelvebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtwelveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamtwelveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamtwelvebackend/TeamtwelveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamtwelvebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamtwelveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
