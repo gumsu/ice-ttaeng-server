f3afc51d6575f3e88f75454699075e380ca844d2
dsyang <neoanimato@gmail.com>
2023-05-12 16:41:02 +0900

    JPA 초기 설정과 CI/CD 환경 변수 설정을 업데이트 했습니다.
    yml 설정 파일을 한개로 관리하기 위해 환경 별로 달라지는 값은 환경 변수화 했습니다.
    로컬 실행 시에도 환경 변수 설정으로 실행하는 것을 권장합니다.

--- README.md
+++ README.md
@@ -6,7 +6,18 @@ build: gradle > build > bootJar
 
 build 이후 docker run
 
+### 로컬 데이터베이스 설정 방법 (Intellij IDEA)
+![img.png](img.png)
+Edit Configurations 로 진입
+
+![img_1.png](img_1.png)
+Environments variables 에 값을 입력. 맨 오른쪽에 문서 아이콘을 누르면 입력 도구가 열림
+
+![img_2.png](img_2.png)
+필요한 환경 변수 값을 로컬 환경에 맞게 입력
+
 ## 기술 스택
 
 - Java 17
 - Spring Boot 3.0.6
+- PostgreSQL 14
--- src/main/java/com/example/teamtwelvebackend/TeamtwelveBackendApplication.java
+++ src/main/java/com/example/teamtwelvebackend/TeamtwelveBackendApplication.java
@@ -1,13 +1,45 @@
 package com.example.teamtwelvebackend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
 
 @SpringBootApplication
+@Slf4j
 public class TeamtwelveBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeamtwelveBackendApplication.class, args);
     }
 
+
+    /**
+     * 현재 설정되어 있는 환경 변수 값을 확인하기 위한 로그 출력 코드 입니다.
+     */
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        final Environment env = event.getApplicationContext()
+                .getEnvironment();
+
+        log.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+
+        final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+
+        StreamSupport.stream(sources.spliterator(), false)
+                .filter(ps -> ps instanceof EnumerablePropertySource)
+                .map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
+                .flatMap(Arrays::stream)
+                .distinct()
+                .filter(prop -> !(prop.contains("credentials") || prop.contains("password")))
+                .forEach(prop -> log.info("{}: {}", prop, env.getProperty(prop)));
+    }
 }
