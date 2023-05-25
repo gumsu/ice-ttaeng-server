b95e1a04723db3660791b1d821243f043a8c5a10
dsyang <neoanimato@gmail.com>
2023-05-25 21:09:41 +0900

    웹소켓 샘플 코드를 추가했습니다.
    스프링 공식 가이드 https://spring.io/guides/gs/messaging-stomp-websocket/ 를 참고했습니다.

---
+++ src/main/java/com/example/teamtwelvebackend/ws/sample/Greeting.java
@@ -0,0 +1,18 @@
+package com.example.teamtwelvebackend.ws.sample;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
