50977b7f85d160809d47886cb4df3542b07d1244
dsyang <neoanimato@gmail.com>
2023-05-29 17:43:58 +0900

    웹소켓 CORS 설정 환경변수화

--- src/main/java/com/example/teamtwelvebackend/WebSocketConfig.java
+++ src/main/java/com/example/teamtwelvebackend/WebSocketConfig.java
@@ -1,14 +1,19 @@
 package com.example.teamtwelvebackend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${websocket.allow-origin-patterns}")
+    List<String> originPatterns;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -18,6 +23,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:63342", "https://15-12-fe.netlify.app", "http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns(originPatterns.toArray(String[]::new))
+                .withSockJS();
     }
 }
