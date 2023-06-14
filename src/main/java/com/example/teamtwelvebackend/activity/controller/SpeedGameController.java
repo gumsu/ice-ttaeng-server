fb49f933950ed94a929cf6083c715e8f2e60c5c1
dsyang <neoanimato@gmail.com>
2023-06-14 20:54:25 +0900

    토큰 정보 확인 코드 추가

--- src/main/java/com/example/teamtwelvebackend/activity/controller/SpeedGameController.java
+++ src/main/java/com/example/teamtwelvebackend/activity/controller/SpeedGameController.java
@@ -4,17 +4,22 @@ import com.example.teamtwelvebackend.activity.controller.request.SpeedGameCreate
 import com.example.teamtwelvebackend.activity.controller.response.ActivityCreateResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/activity/speedgame")
 public class SpeedGameController {
     @PostMapping
-    public ResponseEntity<ActivityCreateResponse> createRoom(@RequestBody SpeedGameCreateRequest request) {
+    public ResponseEntity<ActivityCreateResponse> createRoom(JwtAuthenticationToken principal, @RequestBody SpeedGameCreateRequest request) {
+        String username = principal.getToken().getClaim("username"); // username, ex) google_118339889321875083261
+        String name = principal.getName();// UUID, ex) 204c3264-77d5-4ac7-b776-4be9921535ee
         return ResponseEntity.ok().body(new ActivityCreateResponse("sample-name", "sample-code"));
     }
 }
