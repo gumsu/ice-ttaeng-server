843880080f78ed8b7be39b4aa13052fdbb5eaf5d
dsyang <neoanimato@gmail.com>
2023-06-14 20:56:48 +0900

    불필요한 코드 삭제

--- src/main/java/com/example/teamtwelvebackend/activity/service/SpeedGameService.java
+++ src/main/java/com/example/teamtwelvebackend/activity/service/SpeedGameService.java
@@ -1,19 +1,10 @@
 package com.example.teamtwelvebackend.activity.service;
 
-import com.example.teamtwelvebackend.activity.domain.Activity;
-import com.example.teamtwelvebackend.activity.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class SpeedGameService {
 
-    private final ActivityRepository activityRepository;
-
-    public List<Activity> getAllActivity() {
-        return activityRepository.findAll();
-    }
 }
