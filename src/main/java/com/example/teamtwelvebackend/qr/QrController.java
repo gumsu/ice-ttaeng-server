38ceb75ce33c9087e62bb43585b858f401efc4a9
gumsu <49813916+gumsu@users.noreply.github.com>
2023-06-15 16:49:30 +0900

    컴파일 에러 수정 (#26)

--- src/main/java/com/example/teamtwelvebackend/qr/QrController.java
+++ src/main/java/com/example/teamtwelvebackend/qr/QrController.java
@@ -1,6 +1,5 @@
 package com.example.teamtwelvebackend.qr;
 
-import com.example.teamtwelvebackend.QrResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
