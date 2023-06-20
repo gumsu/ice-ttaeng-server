1abe0855314d268e58b5b16e8995e6b722f45e67
dsyang <neoanimato@gmail.com>
2023-06-20 22:41:34 +0900

    cors-pattern 적용

--- src/main/java/com/example/teamtwelvebackend/WebSecurityConfig.java
+++ src/main/java/com/example/teamtwelvebackend/WebSecurityConfig.java
@@ -1,15 +1,23 @@
 package com.example.teamtwelvebackend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Value("${cors.allow-origin-patterns}")
+    private List<String> originPatterns;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -35,4 +43,13 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(originPatterns.toArray(String[]::new));
+            }
+        };
+    }
 }
